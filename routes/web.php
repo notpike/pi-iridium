8bce0cd01f99e6f7ee96a31be4160a322189fa39
notpike <e1000basetx@gmail.com>
2020-03-05 02:45:44 -0500

    adding exec fetures

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', 'HomeController@index')->name('home');
+
+
 Auth::routes();
 //Auth::routes(['register' => false]);
 
