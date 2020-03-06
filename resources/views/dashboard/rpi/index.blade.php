ceb19bf976dc3391cf0921e257e67448a7907749
notpike <e1000basetx@gmail.com>
2020-03-06 01:27:35 -0500

    RPI Controler Finished

--- resources/views/dashboard/rpi/index.blade.php
+++ resources/views/dashboard/rpi/index.blade.php
@@ -15,11 +15,10 @@
             <div class="card">
             <div class="card-header">CPU Temp</div>
             <div class="card-body">
-                {{-- <h5 class="card-title">CPU Temp</h5> --}}
-                {{-- <p class="card-text">{{ $cpuTemp[0] / 1000 }} C</p> --}}
+                <p class="card-text">{{ $cpuTemp[0] / 1000 }} C</p>
 
                 {{-- DEV USE ON QUBESOS --}}
-                <p class="card-text">{{ $cpuTemp[0] }} C</p> 
+                {{-- <p class="card-text">{{ $cpuTemp[0] }} C</p>  --}}
             </div>
             </div>
         </div>
