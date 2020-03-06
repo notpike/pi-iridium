b620e47bac73279a5eda56dc6e145b8dc3210de5
notpike <e1000basetx@gmail.com>
2020-03-05 23:44:45 -0500

    RPI Stats

--- app/Http/Controllers/RpiController.php
+++ app/Http/Controllers/RpiController.php
@@ -13,15 +13,20 @@ class RpiController extends Controller
      */
     public function index() {
         exec('cat /sys/class/thermal/thermal_zone0/temp 2>&1', $cpuTempVar);
-        // exec("", $totalMemVar);
-        // exec("", $usedMemVar);
+        exec("free -m|awk '/^Mem:/{print $2}'", $totalMemVar); // mb
+        exec("free -m|awk '/^Mem:/{print $3}'", $usedMemVar); // mb
+        exec("free -m|awk '/^Swap:/{print $2}'", $totalSwapVar); // mb
+        exec("free -m|awk '/^Swap:/{print $3}'", $usedSwapVar); // mb
+
         exec("df -h 2>&1", $filesystemVar);
 
 
         return view('dashboard.rpi.index', [
             'cpuTemp'     => $cpuTempVar,
-            // 'totalMemory' => $totalMemVar,
-            // 'usedMemory'  => $usedMemVar,
+            'totalMemory' => $totalMemVar,
+            'usedMemory'  => $usedMemVar,
+            'totalSwap'   => $totalSwapVar,
+            'usedSwap'    => $usedSwapVar,
             'filesystem'  => $filesystemVar 
         ]);
     }
--- resources/views/dashboard/rpi/index.blade.php
+++ resources/views/dashboard/rpi/index.blade.php
@@ -5,56 +5,54 @@
     <div class="card-header">RPI Control</div>
 
     <div class="row">
-            <div class="col-sm-6">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">CPU Temp</h5>
-                  <p class="card-text">{{ $cpuTemp[0] / 1000 }} C</p>
-                </div>
-              </div>
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">CPU Temp</h5>
+                <p class="card-text">{{ $cpuTemp[0] / 1000 }} C</p>
+                {{-- <p class="card-text">{{ $cpuTemp[0] }} C</p> DEV USE ON QUBESOS --}}
             </div>
+            </div>
+        </div>
 
-            <div class="col-sm-6">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Filesystem</h5>
-                  <p class="card-text">
-<pre>
-@foreach($filesystem as $data)
-{{ $data }}
-@endforeach
-</pre>  
-                  </p>
-                </div>
-              </div>
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Memory</h5>
+                <p class="card-text">Total: {{ $totalMemory[0] }} MB</p>
+                <p class="card-text">Used: {{ $usedMemory[0] }} MB</p>
+                <p class="card-text">Free: {{ $totalMemory[0] - $usedMemory[0] }} MB</p>
             </div>
-          </div>
+            </div>
+        </div>
+    </div>
 
     <div class="row">
-            <div class="col-sm-6">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">CPU Temp</h5>
-                  <p class="card-text">{{ $cpuTemp[0] }}</p>
-                </div>
-              </div>
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Swap</h5>
+                <p class="card-text">Total: {{ $totalSwap[0] }} MB</p>
+                <p class="card-text">Used: {{ $usedSwap[0] }} MB</p>
+                <p class="card-text">Free: {{ $totalSwap[0] - $usedSwap[0] }} MB</p>
             </div>
+            </div>
+        </div>
 
-            <div class="col-sm-6">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title">Filesystem</h5>
-                  <p class="card-text">
+        <div class="col-sm-6">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Filesystem</h5>
+                <p class="card-text">
 <pre>
 @foreach($filesystem as $data)
 {{ $data }}
 @endforeach
 </pre>  
-                  </p>
-                </div>
-              </div>
+                </p>
             </div>
-          </div>
+            </div>
+        </div>
     </div>
 
 </div>
