334ff1193ec7d5efe8beac923748562b70970703
notpike <e1000basetx@gmail.com>
2020-03-14 14:03:54 -0400

    Clean Up

--- START_LOCAL.sh
+++ START_LOCAL.sh
@@ -4,8 +4,8 @@ echo '== STARTING PI-IRIDIUM SERVICES =='
 echo ''
 
 ### START PI-IRIDIUM SERVICES
-screen -d -m php artisan websockets:serve --host $(hostname -I)
-screen -d -m php artisan serve --host $(hostname -I)
+screen -d -m php artisan websockets:serve
+screen -d -m sudo php artisan serve
 screen -d -m php artisan queue:work --timeout
 
 #LIST SCREEN SESSIONS
