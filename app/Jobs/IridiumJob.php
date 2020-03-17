650e4955b90a155ddaac4c1519f110c014cf302a
notpike <e1000basetx@gmail.com>
2020-03-17 01:46:07 -0400

    Playing with popen() and pipes

--- app/Jobs/IridiumJob.php
+++ app/Jobs/IridiumJob.php
@@ -46,15 +46,18 @@ class IridiumJob implements ShouldQueue
             // sudo iridium-extractor -D 4 software/gr-iridium/examples/rtl-sdr-T.conf | grep "A:OK" > Iridium/output/output3.bits
             $cmd = 'iridium-extractor -D ' 
                     . trim(escapeshellarg($this->init['d']), '\'') 
-                    . ' ' .  env('GR_IRIDIUM') . '/examples/' . trim($this->init['config'], '\'' ) 
-                    . ' ' . '| grep "A:OK" > ' . base_path() . '/' . env('LOOT_CAPTURE') . '/' . trim(escapeshellarg($this->init['filename']),'\'')
-                    . ' 2>&1';
+                    . ' ' .  env('GR_IRIDIUM') . '/examples/' . trim($this->init['config'], '\'' ) . ' 2>&1';
+                    // . ' ' . '| grep "A:OK" > ' . base_path() . '/' . env('LOOT_CAPTURE') . '/' . trim(escapeshellarg($this->init['filename']),'\'')
+                    // . ' 2>&1';
 
             $this->proc = popen($cmd, 'r');
-
             while (!feof($this->proc)) {
                 broadcast(new IridiumBroadcast(fread($this->proc, 4096)));
             }
+
+            // $this->proc = shell_exec($cmd);
+            // broadcast(new IridiumBroadcast($this->proc));
+
 
         //}
     }
